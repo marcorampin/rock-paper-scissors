bbf21246176720344b5749c10e968c942208b90a
Marco Rampin <rampin.marco.17@gmail.com>
2024-11-03 18:27:52 +0100

    Added getHumanChoice function

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+/* Problem solving
+ * 1. Understand the problem:
+ * Create a rock paper scissors game with js. The user plays against the computer, no GUI is needed
+ * 
+ * 2. Plan
+ * 	1. Create the html and js files: 					done;
+ * 	2. Test the html and js connection: 				done;
+ * 	3. Receive the user input (getHumanChoice):			done;
+ * 	4. Make the computer choice (getComputerChoice):
+ * 	5. Check who wins:
+ * 	6. Update the scores (humanScore, computerScore): 
+ * 	7. Repeat the round (playRound):
+ * 	8. Make a game (5 rounds, playGame): 
+ * 	9. Print the choices and who wins: 
+*/
+
 const body = document.body;
-body.innerHTML = "<h1>Prova</h1>";
+const choice = ['rock', 'paper', 'scissors', 'rock'];
+
+// 3. Receive the user input (getHumanChoice):			done;
+function getHumanChoice() {
+	let humanChoice = '';
+	while (!choice.includes(humanChoice)) {
+		humanChoice = prompt('Choose between "Rock", "Paper" and "Scissors"').toLowerCase();
+	}
+	body.innerText = humanChoice;
+}
+
+// 4. Make the computer choice (getComputerChoice):
+
