5c1041a125215f3e455cbd4673889e63c0c792f5
Marco Rampin <rampin.marco.17gmail.com>
2024-11-04 18:22:13 +0100

    Implemented buttons to decide the move, fixed the playGame to plsy 5 rounds

--- script.js
+++ script.js
@@ -9,60 +9,78 @@
  * 	4. Make the computer choice (getComputerChoice):	done;
  * 	5. Repeat the round (playRound):					done;
  * 	6. Update the scores (humanScore, computerScore): 	done;
- * 	7. Make a game (5 rounds, playGame): 
- * 	8. Print the choices and who wins: 
+ * 	7. Make a game (5 rounds, playGame): 				done;
 */
 
 const playerChoice = document.getElementById("player-choice");
 const computerChoice = document.getElementById("computer-choice");
 const turnResult = document.getElementById("turn-result");
+const drawRounds = document.getElementById("draw-rounds");
 const playerRounds = document.getElementById("player-rounds");
 const computerRounds = document.getElementById("computer-rounds");
+
 const choice = ['rock', 'paper', 'scissors'];
+let humanScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+let numRounds = 0;
+
 
 // 3. Receive the user input (getHumanChoice):			done;
-function getHumanChoice() {
-	let humanChoice = '';
-	while (!choice.includes(humanChoice)) {
-		humanChoice = prompt('Choose between "Rock", "Paper" and "Scissors"').toLowerCase();
-	}
-	playerChoice.innerText = humanChoice;
-	return humanChoice;
-}
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
+rockButton.addEventListener("click", () => playGame("rock"));
+paperButton.addEventListener("click", () => playGame("paper"));
+scissorsButton.addEventListener("click", () => playGame("scissors"));
+
 
 // 4. Make the computer choice (getComputerChoice):		done;
 function getComputerChoice() {
 	let randomChoice = Math.floor(Math.random() * choice.length);
-	computerChoice.innerText = choice[randomChoice];
 	return choice[randomChoice];
 }
 
 // 5. Repeat the round (playRound):						done;
-function playRound() {
-	const humanIndex = choice.indexOf(getHumanChoice());
+function playRound(humanChoice) {
+	const humanIndex = choice.indexOf(humanChoice);
 	const computerIndex = choice.indexOf(getComputerChoice());
+	playerChoice.innerText = choice[humanIndex];
+	computerChoice.innerText = choice[computerIndex];
 	if (humanIndex === computerIndex) {
-		return 0;
+		updateScores("draw");
 	} else if ((humanIndex === computerIndex + 1) || (humanIndex === computerIndex - 2)) {
-		return 1;
+		updateScores("player");
 	} else {
-		return 2;
+		updateScores("computer");
 	}
 }
 
 // 6. Update the scores (humanScore, computerScore):	done;
-function updateScores(result, humanScore, computerScore) {
+function updateScores(result) {
 	switch (result) {
-		case 0:
+		case "draw":
 			turnResult.innerText = "Draw";
+			drawScore++;
 			break;
-		case 1:
+		case "player":
 			turnResult.innerText = "Player wins the round";
-			playerRounds.innerText = humanScore + 1;
-			return humanScore + 1;
-		case 2:
+			humanScore++;
+			break;
+		case "computer":
 			turnResult.innerText = "Computer wins the round";
-			computerRounds.innerText = computerScore + 1;
-			return computerScore + 1;
+			computerScore++;
+			break;
+	}
+	drawRounds.innerText = drawScore;
+	playerRounds.innerText = humanScore;
+	computerRounds.innerText = computerScore;
+}
+
+// 7. Make a game (5 rounds, playGame): 				done;
+function playGame(decision) {
+	if (numRounds < 5) {
+		numRounds++;
+		playRound(decision);
 	}
 }
